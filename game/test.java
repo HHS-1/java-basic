d7f9d0db0382672c9b41a0e75658372414888429
HHS-1 <qazplm1021@naver.com>
2024-05-21 16:21:17 +0900

    main

---
+++ game/test.java
@@ -0,0 +1,12 @@
+package game;
+
+public class test {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("유재석");
+		sb.append("강호동");
+		System.out.println(sb);
+	}
+
+}
---
+++ game/test2.java
@@ -0,0 +1,17 @@
+package game;
+
+public class test2 {
+
+	public static void main(String[] args) {
+			
+		new game2_box();
+		
+		
+	}
+
+}
+class game2_box{
+	public game2_box() {
+		
+	}
+}
